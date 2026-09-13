f15a7ee7d89beb358ddca6235a62192b3f7f0840
Higor Matheus Paes <higormatheuspaes11@gmail.com>
2026-09-13 18:33:41 -0300

    chore: projeto inicial (backend Spring Boot + frontend Next.js)

---
+++ backend/src/main/java/com/himapsystems/aceitaai/AceitaaiApplication.java
@@ -0,0 +1,13 @@
+package com.himapsystems.aceitaai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AceitaaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AceitaaiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/himapsystems/aceitaai/AceitaaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himapsystems.aceitaai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AceitaaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
